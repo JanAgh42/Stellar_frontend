dc2a39f962e27d3c7475ebb46e3b5d9cc5380b1f
Makarovai <xmakarovai@stuba.sk>
2023-04-15 20:03:58 +0200

    ids added to own activities

--- app/src/main/java/com/example/stellar/CreateGroupActivity.kt
+++ app/src/main/java/com/example/stellar/CreateGroupActivity.kt
@@ -2,10 +2,76 @@ package com.example.stellar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class CreateGroupActivity : AppCompatActivity() {
+
+    private lateinit var groupExample: LinearLayout
+
+    private lateinit var exampleBackground: LinearLayout
+    private lateinit var exampleIconBackground: LinearLayout
+    private lateinit var exampleIcon: ImageView
+    private lateinit var exampleName: TextView
+    private lateinit var exampleNumMembers: TextView
+
+    private lateinit var groupName: EditText
+
+    private lateinit var groupBkg: LinearLayout
+
+    private lateinit var groupBkgViolet: ImageButton
+    private lateinit var groupBkgOrange: ImageButton
+    private lateinit var groupBkgRed: ImageButton
+    private lateinit var groupBkgBlue: ImageButton
+    private lateinit var groupBkgGreen: ImageButton
+    private lateinit var groupBkgPink: ImageButton
+    private lateinit var groupBkgPurple: ImageButton
+    private lateinit var groupBkgTurquoise: ImageButton
+
+    private lateinit var groupIconBkg: LinearLayout
+
+    private lateinit var groupIconBkgViolet: ImageButton
+    private lateinit var groupIconBkgOrange: ImageButton
+    private lateinit var groupIconBkgRed: ImageButton
+    private lateinit var groupIconBkgBlue: ImageButton
+    private lateinit var groupIconBkgGreen: ImageButton
+    private lateinit var groupIconBkgPink: ImageButton
+    private lateinit var groupIconBkgPurple: ImageButton
+    private lateinit var groupIconBkgTurquoise: ImageButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_group)
+
+        this.groupName = findViewById(R.id.cgroup_group_name)
+        this.groupExample = findViewById(R.id.cgroup_example_group)
+        this.exampleBackground = groupExample.findViewById(R.id.group_bkg)
+        this.exampleIconBackground = groupExample.findViewById(R.id.group_icon_bkg)
+        this.exampleIcon = groupExample.findViewById(R.id.group_icon)
+        this.exampleName = groupExample.findViewById(R.id.group_name)
+        this.exampleNumMembers = groupExample.findViewById(R.id.group_num_members)
+        this.groupBkg = findViewById(R.id.cgroup_bkg_color)
+        this.groupBkgViolet = groupBkg.findViewById(R.id.group_violet)
+        this.groupBkgOrange = groupBkg.findViewById(R.id.group_orange)
+        this.groupBkgRed = groupBkg.findViewById(R.id.group_red)
+        this.groupBkgBlue = groupBkg.findViewById(R.id.group_blue)
+        this.groupBkgGreen = groupBkg.findViewById(R.id.group_green)
+        this.groupBkgPink = groupBkg.findViewById(R.id.group_pink)
+        this.groupBkgPurple = groupBkg.findViewById(R.id.group_purple)
+        this.groupBkgTurquoise = groupBkg.findViewById(R.id.group_turquoise)
+        this.groupIconBkg = findViewById(R.id.cgroup_icon_bkg_color)
+        this.groupIconBkgViolet = groupIconBkg.findViewById(R.id.group_violet)
+        this.groupIconBkgOrange = groupIconBkg.findViewById(R.id.group_orange)
+        this.groupIconBkgRed = groupIconBkg.findViewById(R.id.group_red)
+        this.groupIconBkgBlue = groupIconBkg.findViewById(R.id.group_blue)
+        this.groupIconBkgGreen = groupIconBkg.findViewById(R.id.group_green)
+        this.groupIconBkgPink = groupIconBkg.findViewById(R.id.group_pink)
+        this.groupIconBkgPurple = groupIconBkg.findViewById(R.id.group_purple)
+        this.groupIconBkgTurquoise = groupIconBkg.findViewById(R.id.group_turquoise)
+
     }
 }
--- app/src/main/java/com/example/stellar/ProfileActivity.kt
+++ app/src/main/java/com/example/stellar/ProfileActivity.kt
@@ -2,10 +2,57 @@ package com.example.stellar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var profilePhoto: CircleImageView
+    private lateinit var profileNotifBtn: ImageButton
+    private lateinit var profileEditPhoto: Button
+    private lateinit var profileLogOut: Button
+    private lateinit var profileUsername: TextView
+    private lateinit var profileNumGroups: TextView
+    private lateinit var profileNumPosts: TextView
+    private lateinit var profileEditBtn: ImageButton
+    private lateinit var profileUsernameField: EditText
+    private lateinit var profileEmailField: EditText
+
+    private lateinit var profileUsernameWhite: ImageButton
+    private lateinit var profileUsernameRed: ImageButton
+    private lateinit var profileUsernameBlue: ImageButton
+    private lateinit var profileUsernameGreen: ImageButton
+    private lateinit var profileUsernamePink: ImageButton
+    private lateinit var profileUsernamePurple: ImageButton
+    private lateinit var profileUsernameYellow: ImageButton
+    private lateinit var profileUsernameTurquoise: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        this.profilePhoto = findViewById(R.id.profile_photo)
+        this.profileNotifBtn = findViewById(R.id.profile_notif_btn)
+        this.profileEditPhoto = findViewById(R.id.profile_edit_photo)
+        this.profileLogOut = findViewById(R.id.profile_log_out)
+        this.profileUsername = findViewById(R.id.profile_username)
+        this.profileNumGroups = findViewById(R.id.profile_num_groups)
+        this.profileNumPosts = findViewById(R.id.profile_num_posts)
+        this.profileEditBtn = findViewById(R.id.profile_edit_btn)
+        this.profileUsernameField = findViewById(R.id.profile_username_field)
+        this.profileEmailField = findViewById(R.id.profile_email_field)
+        this.profileUsernameWhite = findViewById(R.id.profile_username_white)
+        this.profileUsernameRed = findViewById(R.id.profile_username_red)
+        this.profileUsernameBlue = findViewById(R.id.profile_username_blue)
+        this.profileUsernameGreen = findViewById(R.id.profile_username_green)
+        this.profileUsernamePink = findViewById(R.id.profile_username_pink)
+        this.profileUsernamePurple = findViewById(R.id.profile_username_purple)
+        this.profileUsernameYellow = findViewById(R.id.profile_username_yellow)
+        this.profileUsernameTurquoise = findViewById(R.id.profile_username_turquoise)
+
     }
 }
